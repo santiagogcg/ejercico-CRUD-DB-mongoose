08bf494a0d2749352fc0469cc22a313e9603e8dc
Santi <santiagogcg@gmail.com>
2024-05-26 20:08:35 +0200

    SPRINT15 CH2 A CORREGIR

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = 5500;
 const { dbConnection } = require('./config/config');
-const routes = require('./routes');
+const routes = require('./routes/tasks');
 
 app.use(express.json());
 
--- routes/tasks.js
+++ routes/tasks.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const router = express.Router();
 const { Task } = require("../models/Task.js");
+const { error } = require("console");
 
 
 router.post("/create", async (req, res) => {
     try {
         const task = await Task.create(req.body);
         res.status(201).send(task);
+
+
+
     } catch (error) {
         console.error(error);
         res
@@ -14,5 +18,77 @@ router.post("/create", async (req, res) => {
             .send({ message: "There was a problem trying to create a user" });
     }
 });
+
+
+router.get('/', async (req, res) => {
+    try {
+        const tasksAll = await Task.find({});
+        res.status(200).json(tasksAll);
+    } catch (error) {
+        res.status(500).send({ message: "Base data not linked" })
+    }
+
+});
+
+
+router.get('/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const taskID = await Task.findById(id);
+        res.status(200).json(taskID);
+    } catch (error) {
+        res.status(500).send({ message: "Task not found as ID does not exist" })
+    }
+
+});
+
+router.put('/markAsCompleted/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const taskIDUpdate = await Task.findByIdAndUpdate(id, req.body);
+
+        if (!taskIDUpdate) {
+            return res.status(404).json({ message: `Cannot find ${id} suggested` })
+        } else
+            res.status(200).json(taskIDUpdate);
+
+    } catch (error) {
+        res.status(500).send({ message: "error lectura" })
+    }
+
+});
+
+
+router.delete('/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        console.log(id);
+
+        const deletetask = await Task.findOneAndDelete({ _id: id });
+
+        if (deletetask) {
+
+            res.send('Task eliminated ')
+
+        } else
+
+            res.send('ID not found')
+        // if (deletetask === 1) {
+        //     res.status(200).json({ 'mensage:Id matches and task has been deleted'})
+        // } else {
+        //     res.status(500).json({ 'mensage:Id does not matche and no task has  been deleted'})
+
+        // }
+
+    } catch (error) {
+        res.status(500).send({ message: "error lectura" })
+    }
+
+});
+
+
+
+
 
 module.exports = router;
